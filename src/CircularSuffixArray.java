50c46126b8556414252e802c98abb236702c151d
aplyu <plyusch.artem@gmail.com>
2020-04-25 15:20:22 +0300

    add body for circular-suffix-array

---
+++ src/CircularSuffixArray.java
@@ -0,0 +1,19 @@
+package src;
+
+public class CircularSuffixArray {
+
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+
+    }
+
+    // length of s
+    public int length() {
+        return 0;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        return 0;
+    }
+}
