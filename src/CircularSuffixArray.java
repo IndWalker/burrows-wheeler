7b7108fcafd2255324fd838cdce49dfb2763caf7
aplyu <plyusch.artem@gmail.com>
2020-04-25 17:01:22 +0300

    Add CircularSuffix nested class

--- src/CircularSuffixArray.java
+++ src/CircularSuffixArray.java
@@ -16,4 +16,20 @@ public class CircularSuffixArray {
     public int index(int i) {
         return 0;
     }
+
+    private class CircularSuffix implements Comparable<CircularSuffix> {
+        String str;
+        int suffixStart;
+
+        public CircularSuffix(String str, int suffixStart) {
+            this.str = str;
+            this.suffixStart = suffixStart;
+        }
+
+        public int compareTo(CircularSuffix other) {
+            String thisSuffix = this.str.substring(this.suffixStart);
+            String otherSuffix = other.str.substring(other.suffixStart);
+            return thisSuffix.compareTo(otherSuffix);
+        }
+    }
 }
