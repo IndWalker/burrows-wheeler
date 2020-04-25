36900eb4d1591f6a3e0dec452f5f31237cc58c4c
aplyu <plyusch.artem@gmail.com>
2020-04-25 17:27:29 +0300

    Implement CircularSuffixArray

--- src/CircularSuffixArray.java
+++ src/CircularSuffixArray.java
@@ -1,25 +1,39 @@
 package src;
 
+import java.util.Arrays;
+
 public class CircularSuffixArray {
+
+    private CircularSuffix[] circularSuffixes;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
+        if (s == null)
+            throw new IllegalArgumentException("Constuctor argument must not be null");
 
+        circularSuffixes = new CircularSuffix[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            circularSuffixes[i] = new CircularSuffix(s, i);
+        }
+        Arrays.sort(circularSuffixes);
     }
 
     // length of s
     public int length() {
-        return 0;
+        return circularSuffixes.length;
     }
 
     // returns index of ith sorted suffix
     public int index(int i) {
-        return 0;
+        if (i < 0 || i > circularSuffixes.length - 1)
+            throw new IllegalArgumentException("Suffix array index out of bounds");
+
+        return circularSuffixes[i].suffixStart;
     }
 
     private class CircularSuffix implements Comparable<CircularSuffix> {
-        String str;
-        int suffixStart;
+        private String str;
+        private int suffixStart;
 
         public CircularSuffix(String str, int suffixStart) {
             this.str = str;
