d66c897207f432d155a0f7d2686ff2d4f777a342
aplyu <plyusch.artem@gmail.com>
2020-04-25 19:15:45 +0300

    Implement Burrows-Wheeler transform

--- src/BurrowsWheeler.java
+++ src/BurrowsWheeler.java
@@ -1,11 +1,34 @@
 package src;
 
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
 public class BurrowsWheeler {
 
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output
     public static void transform() {
+        while (!BinaryStdIn.isEmpty()) {
+            String text = BinaryStdIn.readString();
+            CircularSuffixArray suffixArray = new CircularSuffixArray(text);
+            int originalStringIndex = 0;
+            for (int i = 0; i < suffixArray.length(); i++) {
+                if (suffixArray.index(i) == 0) {
+                    originalStringIndex = i;
+                    break;
+                }
+            }
 
+            BinaryStdOut.write(originalStringIndex);
+            for (int i = 0; i < suffixArray.length(); i++) {
+                int originalSuffixIndex = suffixArray.index(i);
+                if (originalSuffixIndex == 0)
+                    BinaryStdOut.write(text.charAt(text.length() - 1));
+                else
+                    BinaryStdOut.write(text.charAt(originalSuffixIndex - 1));
+            }
+        }
+        BinaryStdOut.close();
     }
 
     // apply Burrows-Wheeler inverse transform,
@@ -17,6 +40,7 @@ public class BurrowsWheeler {
     // if args[0] is "-", apply Burrows-Wheeler transform
     // if args[0] is "+", apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
-
+        if (args[0].equals("-"))
+            BurrowsWheeler.transform();
     }
 }
