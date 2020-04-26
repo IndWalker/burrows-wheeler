4ab311de93a0676bb2035de92f5be24d7de58e76
aplyu <plyusch.artem@gmail.com>
2020-04-26 14:02:30 +0300

    Implement Burrows-Wheeler inverse transform

--- src/BurrowsWheeler.java
+++ src/BurrowsWheeler.java
@@ -4,6 +4,7 @@ import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
 public class BurrowsWheeler {
+    private static final int ASCII_RADIX = 256;
 
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output
@@ -34,7 +35,55 @@ public class BurrowsWheeler {
     // apply Burrows-Wheeler inverse transform,
     // reading from standard input and writing to standard output
     public static void inverseTransform() {
+        while (!BinaryStdIn.isEmpty()) {
+            int originalStringIndex = BinaryStdIn.readInt();
+            String transformedString = BinaryStdIn.readString();
 
+            /*
+             * Partial key-indexed counting sort algorithm.
+             * Here can be used other sorting algorithms but for this particular task
+             * counting sort is the most effective one with complexity O(n).
+             * Here "partial" algorithm consists of 3 parts and isn't fully identical to original.
+             *
+             * next[i] is the row in sorted order where the (j+1)st original suffix appears.
+             * count[i+1] is the count of character with i-th code.
+             */
+            int[] next = new int[transformedString.length()];
+            int[] count = new int[ASCII_RADIX + 1];
+            /*
+             * Counting part
+             */
+            for (int i = 0; i < transformedString.length(); i++) {
+                count[transformedString.charAt(i) + 1]++;
+            }
+            /*
+             * Calculating cumulative counts part.
+             * Tells how the keys will be distributed.
+             */
+            for (int i = 1; i < count.length - 1; i++) {
+                count[i + 1] += count[i];
+            }
+            /*
+             * Final part. Consists of several steps:
+             * 1) Getting sorted order position of i-th char
+             * 2) Shifting in order to put the next same char to the next cell
+             * 3) filling "next" with (j+1)st original suffix position
+             */
+            for (int i = 0; i < transformedString.length(); i++) {
+                int sortedOrderPosition = count[transformedString.charAt(i)];
+                count[transformedString.charAt(i)]++;
+                next[sortedOrderPosition] = i;
+            }
+
+            int currentIndex = originalStringIndex;
+            for (int i = 0; i < next.length - 1; i++) {
+                char origStringChar = transformedString.charAt(next[currentIndex]);
+                BinaryStdOut.write(origStringChar);
+                currentIndex = next[currentIndex];
+            }
+            BinaryStdOut.write(transformedString.charAt(originalStringIndex));
+        }
+        BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
@@ -42,5 +91,7 @@ public class BurrowsWheeler {
     public static void main(String[] args) {
         if (args[0].equals("-"))
             BurrowsWheeler.transform();
+        else if (args[0].equals("+"))
+            BurrowsWheeler.inverseTransform();
     }
 }
